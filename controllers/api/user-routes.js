b75e26eff87b419dd38430ba553d9423947b6475
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-01-22 22:46:14 -0600

    additional login features

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,18 @@
+const { User } = require("../../../../../module-work/module-14/models");
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
+    
+    
+  });
