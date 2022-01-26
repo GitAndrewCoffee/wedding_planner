70467286d6a06c04d97aa991697084da9083b166
Michael W Macbook <mwegter95@gmail.com>
2022-01-25 20:40:42 -0700

    made stylesheet references in handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { Event_item, Event, User, Wedding } = require('../models/index');
 const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
   console.log("The root page has been called with a get");
-  res.render('homepage');
+  res.redirect('/events');
 });
 
 router.get('/login', (req, res) => {
