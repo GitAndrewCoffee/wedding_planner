e7a90d17a4e3f2cee19b299a809d1ab1ca1cd80d
Michael W Macbook <mwegter95@gmail.com>
2022-01-25 17:58:30 -0700

    events are shown for logged in user, but only on /api/events, want /events instead

--- controllers/api/event-routes.js
+++ controllers/api/event-routes.js
@@ -19,9 +19,31 @@ router.post('/', withAuth, (req, res) => {
     
   });
 
-  router.get("/", (req, res) => {
-    console.log('event get route called')  
-    res.json('Testing');
+  // get all of a user's events for the My Calendar page
+  router.get("/", withAuth, (req, res) => {
+    console.log('event get route called');
+    Event.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'title',
+            'start_time',
+            'end_time'
+        ] 
+    })
+        .then(dbEventData => {
+            const events = dbEventData.map(event => event.get({ plain: true }));
+
+            res.render('events', {
+                events
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        
+        });
   });
 
   module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
+const myCalendarRoutes = require('./my-calendar-routes.js')
 const apiRoutes = require('./api/');
 
 router.use('/', homeRoutes);
+router.use('/events', myCalendarRoutes)
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
