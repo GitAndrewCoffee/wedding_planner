e27b8526837971c5382f4c4106f0857091eb4bf4
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-01-29 21:37:29 -0600

    Added post route for event items

--- public/javascript/event_notes.js
+++ public/javascript/event_notes.js
@@ -1,25 +1,28 @@
-// async function eventFormHandler(event) {
-//   event.preventDefault();
+async function eventFormHandler(event) {
+  event.preventDefault();
 
-//   const title = document.querySelector('#event-title').value.trim();
-//   //const eventNotes = document.querySelector('input[name="event-notes"]').value;
+  const title = document.querySelector('#eItem-Title').value.trim();
+  const notes = document.querySelector('#eItem-Notes').value.trim();
+  const event_id = document.querySelector('#eventID').value.trim();
+  //const eventNotes = document.querySelector('input[name="event-notes"]').value;
 
-//   const response = await fetch(`/api/eventsnotes/`, {
-//     method: 'post',
-//     body: JSON.stringify({
-//       title,
-//       //eventNotes
-//     }),
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   });
+  const response = await fetch(`/api/eventsnotes/`, {
+    method: 'post',
+    body: JSON.stringify({
+      title,
+      notes,
+      event_id
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
-//   if (response.ok) {
-//     document.location.replace('/events');
-//   } else {
-//     alert(response.statusText);
-//   }
-// }
+  if (response.ok) {
+    document.location.replace('/events');
+  } else {
+    alert(response.statusText);
+  }
+}
 
-// document.querySelector('.new-event-form').addEventListener('submit', eventFormHandler);
+document.querySelector('.new-eItem-form').addEventListener('submit', eventFormHandler);
